adc7d1f052b8defb281532610e581566c6ecc7bd
Harosich1 <godvla91@gmail.com >
2020-12-14 19:24:04 +0600

    Добавил избранные

--- app/src/main/java/kz/kolesateam/confapp/favourite_events/presentation/FavouriteEventsActivity.kt
+++ app/src/main/java/kz/kolesateam/confapp/favourite_events/presentation/FavouriteEventsActivity.kt
@@ -2,11 +2,20 @@ package kz.kolesateam.confapp.favourite_events.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kz.kolesateam.confapp.R
+import kz.kolesateam.confapp.favourite_events.domain.FavouritesRepository
+import org.koin.android.ext.android.inject
 
 class FavouriteEventsActivity : AppCompatActivity() {
+
+    private val eventsFavouritesRepository: FavouritesRepository by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourite_events)
+
+        val textView: TextView = findViewById(R.id.favouriteTextView)
+        textView.text = eventsFavouritesRepository.getAllFavouriteEvents().toString()
     }
 }
